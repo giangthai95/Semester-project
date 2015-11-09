1c0e7ad996b8af54d61493763cb4459db32869fb
giangthai95 <gthai60@gmail.com>
2015-11-09 17:39:08 +0000

    testing

--- project.js
+++ project.js
@@ -1,5 +1,13 @@
 /* trying out to se what will help me get started so far still stuck on how to convert the Formula
 Math.E;
+
 function GetE(){
 } 
-    var sum = 0; */
+    var sum = 0; 
+    
+ for in ( var E = 0 => 20){
+ };
+    
+    
+    
+    */
